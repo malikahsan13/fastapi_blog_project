bd312141ff82afbff66c3c5dc8a7e82a29d571a2
Malik Ahsan <malikahsan13@gmail.com>
2026-03-30 22:19:54 +0500

    setup db connection

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+DATABASE_URL = "sqlite:///.blog.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

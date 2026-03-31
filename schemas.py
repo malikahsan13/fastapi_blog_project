6d46c4c2f2dc5676ce5ceb13fcf88c432f5793d3
Malik Ahsan <malikahsan13@gmail.com>
2026-03-31 22:53:20 +0500

    added schems

---
+++ schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+    
+class UserLogin(BaseModel):
+    email: str
+    password: str
+    
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+    
+    class Config:
+        orm_mode = True   # Return ORM model completely
+        
+class PostCreate(BaseModel):
+    title: str
+    content: str
+    
+class PostOut(BaseModel):
+    id: int
+    title: str
+    content: str
+    author: str
+    
+    class Config:
+        orm_mode = True

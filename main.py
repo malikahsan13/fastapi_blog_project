631e6b31bab0355552d4179c46aeed80f5316da4
Malik Ahsan <malikahsan13@gmail.com>
2026-03-31 23:12:22 +0500

    added main file and added middlewares

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from fastapi-blog-project!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import models
+import database
 
 
-if __name__ == "__main__":
-    main()
+models.Base.metadata.create_all(bind = database.engine)
+
+app = FastAPI(title="My Blog App")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to My Blog"}

f05a751aff4390637c3c120585b26ebc6eb53e87
Malik Ahsan <malikahsan13@gmail.com>
2026-04-01 23:30:55 +0500

    added login endpoint

--- auth.py
+++ auth.py
@@ -24,3 +24,14 @@ def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.post("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+    
+    if not check_password_hash(db_user.password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+        

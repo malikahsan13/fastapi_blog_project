87f14e3f0a0dd5ee05e4b2aa6b44490136489f36
Malik Ahsan <malikahsan13@gmail.com>
2026-04-01 23:24:04 +0500

    added signup endpoint in auth

--- auth.py
+++ auth.py
@@ -2,9 +2,25 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 import schemas, models, utils
 from database import get_db
-from wekzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/signup", response_model=schemas.UserOut)
-def signup(user: schemas.UserCreate, db: Session = Depends(get_db))
+def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    hashed_password = generate_password_hash(user.password)
+    
+    new_user = models.User(
+        username = user.username,
+        email = user.email,
+        password = hashed_password
+    )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user

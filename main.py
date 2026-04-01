73f4be92e42c5309247cc33fdcee2d13d3c3db95
Malik Ahsan <malikahsan13@gmail.com>
2026-04-01 23:33:52 +0500

    added auth router in main file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import models
 import database
+from auth import router as auth_router
 
 
 models.Base.metadata.create_all(bind = database.engine)
@@ -15,6 +16,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+app.include_router(auth_router)
 
 @app.get("/")
 def root():

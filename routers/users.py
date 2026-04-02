4e90eb3c731104876a33553e77edb5a4e556b5cd
Malik Ahsan <malikahsan13@gmail.com>
2026-04-02 21:13:06 +0500

    created get all users endpoint in users router

---
+++ routers/users.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+import models, schemas
+from database import get_db
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.get("/", response_model=List[schemas.UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
